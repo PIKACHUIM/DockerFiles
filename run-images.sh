994e25a18b843db2b6be446651d4e8cb2b242de7
PIKACHUIM <pikachuim@qq.com>
2023-02-01 09:51:46 +0000

    Ubuntu 22 Images OK

---
+++ run-images.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+echo    ┌──────────────────────────────────────────────────────────────────────┐
+echo    │         Pikachu Docker Image Build and Container Setup Tool          │
+echo    │                     Last Updated Feb 01 / 2023                       │
+echo    │            Copyright © 2023 Pikachu, All Rights Reserved             │
+echo    └──────────────────────────────────────────────────────────────────────┘
+echo 
+echo 
+echo     ===========================Available Images===========================
+echo         [1] ubuntu
+echo     ======================================================================
+echo 
+echo -n "Enter Docker OS Type(ubuntu): "
+read OS_TYPE
+echo 
+echo 
+echo     ===========================Available Version==========================
+echo         [1] 22.04
+echo     ======================================================================
+echo -n "Enter Docker Version(22.04): "
+read VERSION
+# -----------------------------------------------------------------------------
+echo     ==========================Config Port Mapping=========================
+echo -n "请输入容器ID，留空需要手动配置端口  "
+echo    "Leave blank to manually configure!"
+echo -n "Enter Docker ID(Length=2 Like 01): "
+read USE_PID
+if [ ! $USE_PID ]; then
+  echo "Note: You need manually set port mapping!"
+  PORTMAP=""
+  IN_PORT=""
+else
+  echo "=================Auto Set Detail=================="
+  PM_SSHS="1${USE_PID}22";
+  IN_PORT="q";
+  D_NAMES="docker${USE_PID}"
+  TMP1MAP="-p 1${USE_PID}01-1${USE_PID}21:1${USE_PID}01-1${USE_PID}21";
+  TMP2MAP="-p 1${USE_PID}23-1${USE_PID}99:1${USE_PID}23-1${USE_PID}99";
+  PORTMAP="${TMP1MAP} ${TMP2MAP}";
+  echo "Note: Port Mapping: $TMP1MAP";
+  echo "Note: Port Mapping: $TMP2MAP";
+  echo "Note: SSH Port Use: $PM_SSHS";
+  echo "Note: Docker Names: $D_NAMES";
+  echo "==================================================";
+fi
+# -----------------------------------------------------------------------------
+while [ ! $PM_SSHS ];
+do
+  echo -n "Enter SSH Port Mapping(XXX): "
+  read PM_SSHS
+  if [ ! $PM_SSHS ]; then
+    echo "Error: You must enter ssh new port!!"
+  else
+    break
+  fi
+done
+# -----------------------------------------------------------------------------
+if [ "$IN_PORT" != "q" ]; then
+    echo "==============Manually Configure================="
+    echo "Note: !!!Enter 'q' to Finish Port Mapping Input!!!"
+fi
+while [ "$IN_PORT" != "q" ];
+do
+  while [ "$IN_PORT" == "" ];
+  do
+    echo -n "Enter Port Mapping(XXX:XXX): "
+    read IN_PORT
+    if [ ! $IN_PORT ]; then
+      echo "Error: You need enter port map, or q to exit!!!"
+    else
+        break
+    fi
+  done
+  if [ "$IN_PORT" != "q" ]; then
+    PORTMAP="${PORTMAP} -p ${IN_PORT} "
+  else
+    break
+  fi
+  IN_PORT=""
+done
+# -----------------------------------------------------------------------------
+while [ ! $D_NAMES ];
+do
+  echo -n "Enter Docker Names(XXXXXXX): "
+  read D_NAMES
+  if [ ! $D_NAMES ]; then
+    echo "Error: You must enter container name"
+  else
+        break
+  fi
+done
+# NULL Data ---------------------------------
+if [ ! $OS_TYPE ]; then
+  echo Note: OS_TYPE='ubuntu'
+  OS_TYPE='ubuntu'
+fi
+if [ ! $VERSION ]; then
+  echo Note: VERSION='22.04'
+  VERSION='22.04'
+fi
+
+# Build Images ------------------------
+sudo docker rmi $OS_TYPE:$VERSION-temp
+sudo docker build . \
+--build-arg OS_VERSION=22.04 \
+--build-arg OS_SYSTEMS=ubuntu \
+-t $OS_TYPE:$VERSION-temp
+
+# RUN Images --------------------------
+sudo docker run -itd --privileged \
+--name $D_NAMES \
+-h $D_NAMES-cd1.pika.wiki \
+$PORTMAP \
+-p $PM_SSHS:22 \
+$OS_TYPE:$VERSION-temp
+# Message -----------------------------
+PORTMAP=${PORTMAP//-\\n\\n\\n}
+PORTMAP=${PORTMAP//-p/\\n\\t}
+D_PASSW=$(openssl rand -hex 12)
+
+# Password and Output -------------------------------------------------------
+docker exec $D_NAMES /bin/bash -c "echo root:${D_PASSW} | chpasswd"
+echo    ─────────────────────────────────────────────────────────────────────
+echo                    Docker Container Create Successfully                  
+echo        
+echo                       Container $D_NAMES                                                                 
+echo                       OSSystem: $OS_TYPE $VERSION                            
+echo                       SSH Port: v4-cd1.pika.wiki:$PM_SSHS
+echo                       Username: root         
+echo                       Password: $D_PASSW  
+echo    
+echo                    Port Mapping Details:
+echo -e                       $PORTMAP    
+echo    ─────────────────────────────────────────────────────────────────────     
+echo Password: $D_NAMES $D_PASSW >> ~/docker-user.conf
+
