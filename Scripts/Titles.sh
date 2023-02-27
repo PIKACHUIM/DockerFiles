40362f132ca911b261587e188f7fbb50089c7a6e
PIKACHUIM <pikachuim@qq.com>
2023-02-27 05:37:15 +0000

    Updated at 2023-02-27 05:37:14+00:00

---
+++ Scripts/Titles.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+clear
+echo
+echo "   ┌──────────────────────────────────────────────────────────────────────┐"
+echo "   │          Pikachu Docker Image Build and Container Setup Tool         │"
+echo "   │                     Last Updated Feb 25 / 2023                       │"
+echo "   │            Copyright © 2023 Pikachu, All Rights Reserved             │"
+echo "   └──────────────────────────────────────────────────────────────────────┘"
+echo
