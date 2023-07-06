c1e9f8f0df18744727203eaa63f68ba29483d936
PIKACHUIM <pikachuim@qq.com>
2023-07-06 13:34:41 +0800

    Updated at 2023-07-06 13:34:41+08:00

--- Scripts/Create.sh
+++ Scripts/Create.sh
@@ -4,7 +4,7 @@ source Scripts/Create/Select-Systems.sh
 source Scripts/Create/Select-Desktop.sh
 source Scripts/Nvidia.sh
 source Scripts/Number.sh
-mkdir -p "${DATAPATH}${PV_DATA}"
+sudo mkdir -p "${DATAPATH}${PV_DATA}"
 # RUN Images ---------------------------------------------------------------------
 echo -n "   Docker: "
 sudo docker run -itd \
@@ -15,16 +15,16 @@ $GPU_LIST \
 --cap-add SYS_ADMIN \
 --cap-add=SYS_PTRACE \
 -h $D_NAMES.$HOSTNAME \
-$PORTMAP \
+   $PORTMAP \
 -p $PM_SSHS:22 \
 -p $PM_NXSR:4000 \
 -p $PM_VNCS:5900 \
--v "${DATAPATH}${PV_DATA}:/home/user"
+-v "${DATAPATH}${PV_DATA}:/home/user" \
 pikachuim/$OS_TYPE:$VERSION-$GUI_ENV
 sudo docker exec $D_NAMES /bin/bash -c "systemctl daemon-reload"
 echo -n "   "
 sudo docker exec $D_NAMES /bin/bash -c "systemctl enable run" >> /dev/null
-echo -n "   Docker Restarting Container: "
+echo -n "   Docker Restarting Container: 
 sudo docker restart $D_NAMES
 echo "   ==========================Enter Key to Continue========================="
 read KEY
@@ -46,15 +46,19 @@ echo "                      SSHLogin: v4-cd1.pika.wiki:$PM_SSHS    "
 echo "                      NXServer: v4-cd1.pika.wiki:$PM_NXSR    "
 echo "                      IPV4Host: $D_NAMES.$IPV4HOST    " 
 echo "                      IPV6Host: $D_NAMES.$IPV6HOST    " 
-echo "                      HostName: $D_NAMES.$HOSTNAME    " 
+echo "                      HostName: $D_NAMES.$HOSTNAME    "
 echo "     ----------------------------------------------------------------------"
 echo "                      Username: root                         " 
 echo "                      Password: $D_PASSW                     " 
 echo "                      Username: user                         " 
 echo "                      Password: $D_PASSW                     " 
 echo "     ----------------------------------------------------------------------" 
-echo "     Port Mapping Details:                                   " 
-echo -e "                         $PORTMAP_TEXT                    " 
+echo "     Port Mapping Details:" 
+echo -e "            $PORTMAP_TEXT" 
+echo ""
+echo "     Container Volume Map:" 
+echo ""
+echo -e "         Host: ${DATAPATH}${PV_DATA} -> OCI: /home/user" 
 echo "     ----------------------------------------------------------------------" 
 echo "     Note: Saved password in ~/docker-users.conf, delete if no need backup!"
 echo "     For any questions or suggestions, please visit:                       " 
